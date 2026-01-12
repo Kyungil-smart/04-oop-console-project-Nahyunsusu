011fe055e879d388024464f9893fa88e44d7a1c7
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-01-12 09:33:47 +0900

    불필요한 클래스 삭제

--- Pratice/obj/Debug/net10.0/Pratice.AssemblyInfo.cs
+++ Pratice/obj/Debug/net10.0/Pratice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pratice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2ab99e36d37e65690d359307a4631f6cf4413c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c828f0afe56ac85025ac41205f269bb0d61a281d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pratice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pratice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
